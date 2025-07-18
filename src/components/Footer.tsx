a998809a98ca16b6274bd8bcba77588d9f4fb806
Anshul Anand <anandanshul001@gmail.com>
2025-07-18 13:54:16 +0530

    feat: meet the co-founders

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,11 +8,10 @@ const Footer = () => {
       className='flex flex-col justify-center items-center bg-secondary_bg_color px-4 py-32 text-center'
     >
       <div className='flex justify-between items-start flex-col lg:flex-row gap-8 max-w-7xl w-full'>
-        <div className='text-left'>
+        <div className='text-left max-w-96'>
           <h1 className='text-slate-300 text-2xl font-semibold'>GPA Master</h1>
           <p className='text-slate-400 text-base'>
-            GPA Master aims at providing help and guidance to students with{' '}
-            <br />
+            GPA Master aims at providing help and guidance to students with
             resources and community building.
           </p>
           <div className='flex gap-2 mt-4'>
@@ -32,7 +31,7 @@ const Footer = () => {
             </a>
           </div>
         </div>
-        <div className='flex flex-col md:flex-row gap-6 md:gap-16'>
+        <div className='flex flex-col md:flex-row gap-6 md:gap-16 lg:gap-12'>
           <ul className='flex flex-col text-left text-slate-400'>
             <li className='text-slate-300 font-bold'>Contact</li>
             <li>
@@ -76,6 +75,21 @@ const Footer = () => {
               href='https://merchant.razorpay.com/policy/Og51W3JFp4hAAO/contact_us'
             >
               Contact Us
+            </a>
+          </ul>
+          <ul className='flex flex-col text-left text-slate-400'>
+            <li className='text-slate-300 font-bold'>Made with &#x1F90D; by</li>
+            <a
+              target='_blank'
+              href='https://www.linkedin.com/in/shivam-rajput57/'
+            >
+              Shivam Rajput
+            </a>
+            <a
+              target='_blank'
+              href='https://www.linkedin.com/in/anshulanand02/'
+            >
+              Anshul Anand
             </a>
           </ul>
         </div>
